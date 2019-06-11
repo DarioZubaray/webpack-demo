89a42c59493027d716bc111635855fdf3d178adb
Dario Zubaray <dzubaray@gmail.com>
2019-06-11 14:38:41 -0300

    agregando el html-webpack-plugin para llevar el index.html a la carpeta dist con el requerimiento automatico del script bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: './index.html' //relative to root of the application
+    })
+ ]
+
 };
